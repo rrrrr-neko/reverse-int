1ad7140ac97b6384ecc875338878b407b7796ee5
Svetlana Konyaeva <skonyaeva@list.ru>
2024-08-16 13:29:53 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let res = '';
+    n = n.toString();
+    if ( n < 0) {
+        n = n.slice(1);
+    }
+    res += n.split('').reverse().join('');
+    return +res;
 }
